221d32f4b84741979ade1db10f826c9921da5bb7
AlexaKhiteva <alexakhiteva@inbox.ru>
2023-04-18 11:55:49 +0300

    четвертое задание выполнено полностью

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,47 @@
 function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.subject = null;
+    this.marks = [];
+  }
   
-}
+  Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
+  };
+  
+  Student.prototype.addMarks = function (...marksToAdd) {
+    if (!this.marks) {
+      console.log("Student is excluded!");
+      return;
+    }
+    this.marks.push(...marksToAdd);
+  };
+  
+  Student.prototype.getAverage = function () {
+    if (!this.marks || this.marks.length === 0) {
+      return 0;
+    }
+    const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+    return sum / this.marks.length;
+  };
+  
+  Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  };
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
+ 
+  const student1 = new Student("Василиса", "женский", 19);
+  const student2 = new Student("АртЁм", "мужской", 25);
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
 
-Student.prototype.getAverage = function () {
-  
-}
+  student1.addMarks(5, 4, 5);
+  student1.setSubject("Algebra");
+  student1.addMarks(4, 5, 4, 5);
+  console.log(student1.getAverage());
 
-Student.prototype.exclude = function (reason) {
-  
-}
+ 
+  student2.exclude("Geometry");
+  console.log(student2.excluded);
